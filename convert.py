3972d8b38a987c3456dc6ff1ddec827bfecc0571
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2024-01-06 00:50:23 +0100

    Update: Add switches for `emph` usage and `fancyvrb` package

--- convert.py
+++ convert.py
@@ -1,3 +1,5 @@
+#pylint: disable=E0203,E1101
+
 import sys
 import argparse
 import yaml
@@ -14,11 +16,15 @@ class App:
     
     @classmethod
     def _get_parser(cls):
+        ON_OFF = ["ON", "OFF"]
+
         parser = argparse.ArgumentParser()
         parser.add_argument("input", action="store", nargs="?")
         parser.add_argument("-o", "--output", action="store", default=None)
         parser.add_argument("-e", "--escape", action="store", dest="escape_characters")
         parser.add_argument("-v", "--verbose", action="store_true")
+        parser.add_argument("--use-emph", action="store", nargs='*', choices=["single", "double"], dest="use_emph")
+        parser.add_argument("--pkg-fancyvrb", action="store", choices=ON_OFF)
         parser.set_defaults(**cls._read_defaults())
         return parser
 
@@ -26,18 +32,31 @@ class App:
     def _parse_arguments(args):
         parser = App._get_parser()
         namespace = parser.parse_args(args)
+
         if not namespace.input.lower().endswith(".md"):
             raise ValueError(
                 f'Input file "{namespace.input}" must end in ".md"'
             )
         if namespace.output is None:
             namespace.output = namespace.input[:-3] + '.tex'
+
+        for key, value in vars(namespace).items():
+            if value == "ON":
+                setattr(namespace, key, True)
+            elif value == "OFF":
+                setattr(namespace, key, False)
+        
+        namespace.cmd_single = ("emph" if "single" in namespace.use_emph else "textit")
+        namespace.cmd_double = ("emph" if "double" in namespace.use_emph else "textbf")
+        namespace.env_verbatim = ("Verbatim" if namespace.pkg_fancyvrb else "verbatim")
+
         return vars(namespace)
         
     @staticmethod
     def _read_defaults():
         with open("defaults.yaml", "r", encoding="utf-8") as f:
             return yaml.safe_load(f)
+
 
 class MarkdownParser:
 
@@ -68,19 +87,19 @@ class MarkdownParser:
     @staticmethod
     def block_code(text):
         """Block literal code"""
-        # Inline literal code
-        text = re.sub(r"(?<![`\\])`([^`]+?)(?<!\\)`(?!`)", r"\\texttt{\1}", text)
-        
-        # Block literal code
+        env_verbatim = app.env_verbatim
         pattern = r"^```\n(.*?)\n```$"
-        replace = "\\begin{{verbatim}}\n{}\n\\end{{verbatim}}\n"
+        replace = "\\begin{{{env_verbatim:}}}{arguments:}\n{content:}\n\\end{{{env_verbatim}}}\n"
         while True:
             match_ = re.search(pattern, text, flags=DOTALL+MULTILINE)
             if match_ is None:
                 break
             start, end = match_.span()
             content = match_.groups()[0]
-            text = text[:start] + replace.format(indent(content, " "*4)) + text[end:]
+            text = text[:start] + replace.format(env_verbatim=env_verbatim,
+                                                 arguments="",
+                                                 content=indent(content, " "*4),
+                                                 ) + text[end:]
         return text
     
     @staticmethod
@@ -113,10 +132,12 @@ class MarkdownParser:
     
     @staticmethod
     def emph(text):
-        text = re.sub(r"(?<!\*)\*{2}(\w[^\*\n]*?\w)\*{2}(?!\*)", r"\\{{textbf}}{{\1}}", text)
-        text = re.sub(r"(?<!_)_{2}(\w[^\_\n]*?\w)_{2}(?!_)", r"\\{{textbf}}{{\1}}", text)
-        text = re.sub(r"(?<!\*)\*{1}(\w[^\*\n]*?\w)\*{1}(?!\*)", r"\\{{textit}}{{\1}}", text)
-        text = re.sub(r"(?<!_)_{1}(\w[^\*\n]*?\w)_{1}(?!_)", r"\\{{textit}}{{\1}}", text)
+        cmd_double = app.cmd_double
+        cmd_single = app.cmd_single
+        text = re.sub(r"(?<!\*)\*{2}(\w[^\*\n]*?\w)\*{2}(?!\*)", rf"\\{cmd_double}{{\1}}", text)
+        text = re.sub(r"(?<!_)_{2}(\w[^\_\n]*?\w)_{2}(?!_)", rf"\\{cmd_double}{{\1}}", text)
+        text = re.sub(r"(?<!\*)\*{1}(\w[^\*\n]*?\w)\*{1}(?!\*)", rf"\\{cmd_single}{{\1}}", text)
+        text = re.sub(r"(?<!_)_{1}(\w[^\*\n]*?\w)_{1}(?!_)", rf"\\{cmd_single}{{\1}}", text)
         return text
     
     @staticmethod
@@ -126,12 +147,14 @@ class MarkdownParser:
         # List of spans of verbatims in text
         verbatims = []
 
+        env_verbatim = app.env_verbatim
+
         # Positions in text to be escaped
         escape_positions = set()
 
         # Populate verbatims
         # Ignoring case to also capture environment `Verbatim` from package `fancyvrb`
-        for verbatim in re.finditer(r"\\begin{verbatim}.+?\\end{verbatim}", text, flags=DOTALL+IGNORECASE):
+        for verbatim in re.finditer(rf"\\begin{env_verbatim}.+?\\end{env_verbatim}", text, flags=DOTALL+IGNORECASE):
             verbatims.append(verbatim.span())
 
         # Populate escape_positins (for all escape characteres)
